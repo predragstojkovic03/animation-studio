b3976930381bd4c5e67c105d7bba86b913d689be
Predrag Stojković <56862233+predragstojkovic03@users.noreply.github.com>
2023-02-26 14:08:11 +0100

    Greska

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import styles from './Hero.module.css';
 
 const Hero = () => {
   return (
-    <div className='container'>
+    <div className={`container ${styles.hero}`}>
       <div className={styles.content}></div>
     </div>
   );
