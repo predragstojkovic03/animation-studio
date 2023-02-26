c6827cedb50c76063f74742f24e45a5a004e43cb
Predrag Stojković <56862233+predragstojkovic03@users.noreply.github.com>
2023-02-26 14:04:30 +0100

    Created Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,8 +4,16 @@ import styles from './Hero.module.css';
 
 const Hero = () => {
   return (
-    <div className='container'>
-      <div className={styles.content}></div>
+    //"container " + nekaKlasa        ""
+    <div className={`container ${styles.hero}`}>
+      <div className={styles.content}>
+        <h1>OPEN THE DOOR TO THE MAGIC WORLD OF VFX</h1>
+        <p className='lead'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi qui
+          est ipsam eos explicabo ullam maxime a neque fugit nobis.
+        </p>
+        <button className={styles.btn}>Explore</button>
+      </div>
     </div>
   );
 };
